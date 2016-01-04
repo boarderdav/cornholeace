e56ad2094cb6f52b8399afd08474ee44858dc09a
David Jenkins <djenk008@odu.edu>
2016-01-03 22:43:52 -0500

    added cornholeace image to header

--- website/php/navigation.php
+++ website/php/navigation.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">CornholeAce</a>
+            <a class="navbar-brand" href="https://cornholeace.com/"><img src="/website/img/logo.png" style="height=40px" alt="CornholeAce"/></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
