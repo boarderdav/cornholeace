436a823ac3f4cf5d234fba31008fba74e8cbb471
boarderdav <davidgjenkins83@gmail.com>
2016-04-28 11:30:11 -0400

    updated functionstw.php to include proper link to new bluehost database usersTwitter.  Changed to read cornhol2_newcornholeace.usersTwitter

--- website/functionstw.php
+++ website/functionstw.php
@@ -16,14 +16,14 @@ mysqli_select_db($conn, $dbname) or die ("could not open database connection");
 //$fbcover = 'https://graph.facebook.com/' . $_SESSION['FBID'] . '?fields=cover';
 //$fbpicture = 'https://graph.facebook.com/' . $_SESSION['FBID'] . '/picture';
 //$bio = GetSQLValueString($user_profile['bio'], "text");
-$query = "SELECT * FROM usersTwitter WHERE user_id = '{$_SESSION['TID']}'";  //just changed to only $femail instead of dgjenkin@vt.edu
+$query = "SELECT * FROM cornhol2_newcornholeace.usersTwitter WHERE user_id = '{$_SESSION['TID']}'";  //just changed to only $femail instead of dgjenkin@vt.edu
 //echo $_SESSION['TID'];
 //echo $tid;
 $res = mysqli_query($conn, $query) or die('Query failing at $res: ' . mysqli_error($conn) . "<br>\n$sql");
 //echo "Show nubmer of rows returned " . mysqli_num_rows($res) . " !!!";
 if(mysqli_num_rows($res) == 0)
 {
-    $iquery = "INSERT INTO usersTwitter (user_id,name,screen_name,location,profile_image_url,description,geo,place,url,
+    $iquery = "INSERT INTO cornhol2_newcornholeace.usersTwitter (user_id,name,screen_name,location,profile_image_url,description,geo,place,url,
 followers_count)
 					VALUES ('{$_SESSION['TID']}','{$_SESSION['NAME']}','{$_SESSION['SCREENNAME']}','{$_SESSION['location']}','{$_SESSION['PROFILEIMAGE']}','{$_SESSION['DESCRIPTION']}','{$_SESSION['GEO']}','{$_SESSION['PLACE']}','{$_SESSION['FOLLOWERSCOUNT']}','{$_SESSION['FRIENDCOUNT']}')";
     $ires = mysqli_query($conn, $iquery) or die('Query failed: at $ires ' . mysqli_error($conn) . "<br>\n$sql");
