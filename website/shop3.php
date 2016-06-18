c308f60cb0d2da2d0448ee15b37e622c790e0bb9
boarderdav <davidgjenkins83@gmail.com>
2016-06-18 18:14:25 -0400

    structure updates

--- website/shop3.php
+++ website/shop3.php
@@ -6,7 +6,7 @@
 
 <br><br>
 <div class="container">
-    <div class="row">
+    <div class="container">
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 
             <button class="btn btn-success btn-block btn-lg hidden-lg hidden-md"
