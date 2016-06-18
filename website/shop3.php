79d3ed624ed2992d8a023e2a028de9a03aeb708c
boarderdav <davidgjenkins83@gmail.com>
2016-06-18 17:19:01 -0400

    structure updates

--- website/shop3.php
+++ website/shop3.php
@@ -146,8 +146,7 @@
 
 
             <div>
-                <button class="btn btn-block btn-success btn-lg" href="https://tgs.celeryshop
-                .com/checkout?product_id=559c711099d87e030025424e">
+                <button class="btn btn-block btn-success btn-lg" href="https://tgs.celeryshop.com/checkout?product_id=56ce0181a5060a11002be466">
                     Order Now
                 </button>
                 <br>
