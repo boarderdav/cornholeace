aaa29e5e1f814a54afe7345423886a331e54523c
boarderdav <davidgjenkins83@gmail.com>
2016-06-20 19:20:26 -0400

    added zopium chat

--- website/php/head.php
+++ website/php/head.php
@@ -29,6 +29,12 @@
 <script src="https://www.celeryshop.com/sdk/sdk.js"></script>
 
 
+    <!-- Start of tailgatingsports Zendesk Widget script -->
+    <script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(c){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var o=this.createElement("script");n&&(this.domain=n),o.id="js-iframe-async",o.src=e,this.t=+new Date,this.zendeskHost=t,this.zEQueue=a,this.body.appendChild(o)},o.write('<body onload="document._l();">'),o.close()}("//assets.zendesk.com/embeddable_framework/main.js","tailgatingsports.zendesk.com");
+        /*]]>*/</script>
+    <!-- End of tailgatingsports Zendesk Widget script -->
+
+
 <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
 <!--<!--[if lt IE 9]><script src="../../vendor/twitter/bootstrap/docs/assets/js/ie8-responsive-file-warning.js"></script>[endif]-->
 <!--<script src="../../vendor/twitter/bootstrap/docs/assets/js/ie-emulation-modes-warning.js"></script>-->
