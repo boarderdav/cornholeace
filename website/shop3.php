32f645754a439666ddfdec6498d96e976561eeb7
David Jenkins <david.g.jenkins@nasa.gov>
2016-06-21 13:57:58 -0400

    changed .com to .php in folders

--- website/shop3.php
+++ website/shop3.php
@@ -208,12 +208,13 @@
 
             <div class="col-md-3 col-sm-6 hero-feature">
                 <div class="thumbnail">
-                    <a href="http://new.cornholeace.com/bluebag.com"><img src="/img/CornholeBags-RedFinal.jpg" alt=""></a>
+                    <a href="http://new.cornholeace.com/bluebag.php"><img src="/img/CornholeBags-RedFinal.jpg"
+                                                                         alt=""></a>
                     <div class="caption">
                         <h3>Professional Red Cornhole Bags</h3>
                         <p>Four (4) professional red cornhole bags.</p>
                         <p>
-                            <a href="https://tgs.celeryshop.com/checkout?product_id=56ce0181a5060a11002be466" class="btn btn-primary">Buy Now!</a> <a href="http://new.cornholeace.com/shop3.com" class="btn btn-default">More Info</a>
+                            <a href="https://tgs.celeryshop.com/checkout?product_id=56ce0181a5060a11002be466" class="btn btn-primary">Buy Now!</a> <a href="http://new.cornholeace.com/shop3.php" class="btn btn-default">More Info</a>
                         </p>
                     </div>
                 </div>
@@ -228,7 +229,7 @@
                         <h3>Professional Blue Cornhole Bags</h3>
                         <p>Four (4) professional Blue cornhole bags.</p>
                         <p>
-                            <a href="http://new.cornholeace.com/bluebag.com" class="btn btn-primary">Buy Now!</a> <a href="http://new.cornholeace.com/bluebag.com" class="btn btn-default">More Info</a>
+                            <a href="http://new.cornholeace.com/bluebag.php" class="btn btn-primary">Buy Now!</a> <a href="http://new.cornholeace.com/bluebag.php" class="btn btn-default">More Info</a>
                         </p>
                     </div>
                 </div>
